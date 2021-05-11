401542e9e777d3bc9b870e5ec6da3cf1cb684875
merepbj <meredithjohnson76@gmail.com>
2021-05-11 19:28:11 -0400

    save model

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask_pymongo import PyMongo
 from flask import Flask, render_template, redirect
+import pickle
+
 
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri='mongodb://localhost:27017/final_project_db')
+#mongo = PyMongo(app, uri='mongodb://localhost:27017/final_project_db')
+pickle_model = None 
+with open("predict_wins.sav", 'rb') as file:
+    pickle_model = pickle.load(file)
 
